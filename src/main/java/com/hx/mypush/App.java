e46661f35b0c89e0090798fd8859230f34ce097f
zyq <zyq@qq.com>
2016-12-16 17:07:11 +0800

    zdb

---
+++ src/main/java/com/hx/mypush/App.java
@@ -0,0 +1,13 @@
+package com.hx.mypush;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
